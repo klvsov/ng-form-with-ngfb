e0dadf2fe3133d499531cba0b81430d944ade377
klvsov <klvsov@gmail.com>
2023-03-02 15:42:17 +0200

    updated form - added list of fields as props

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,109 @@ export class AppComponent {
   callback() {
     console.log('hello from callback');
   }
+
+  fieldList = [
+    {
+      type: 'text',
+      name: 'name',
+      label: 'Your first name',
+      validations: [
+        {
+          type: 'required',
+          value: true,
+          errorMessage: 'Required field',
+        },
+        {
+          type: 'minLength',
+          value: 2,
+          errorMessage: 'At least 2 characters',
+        },
+        {
+          type: 'pattern',
+          value: '[a-zA-Z]+',
+          errorMessage: 'Only letters',
+        },
+      ],
+    },
+    {
+      type: 'text',
+      name: 'lastName',
+      label: 'Your last name',
+      validations: [
+        {
+          type: 'required',
+          value: true,
+          errorMessage: 'Required field',
+        },
+        {
+          type: 'minLength',
+          value: 2,
+          errorMessage: 'At least 2 characters',
+        },
+        {
+          type: 'pattern',
+          value: '[a-zA-Z]+',
+          errorMessage: 'Only letters',
+        },
+      ],
+    },
+    {
+      type: 'email',
+      name: 'email',
+      label: 'Your email address',
+      placeholder: 'Email address',
+      validations: [
+        {
+          type: 'required',
+          value: true,
+          errorMessage: 'Required field',
+        },
+        {
+          type: 'email',
+          value: true,
+          errorMessage: 'Invalid value',
+        },
+      ],
+    },
+    {
+      isMultiLine: true,
+      name: 'message',
+      label: 'Your comment',
+      validations: [
+        {
+          type: 'required',
+          value: true,
+          errorMessage: 'Required field',
+        },
+      ],
+    },
+    {
+      type: 'password',
+      name: 'password',
+      label: 'Your password',
+      placeholder: 'Password',
+      validations: [
+        {
+          type: 'required',
+          value: true,
+          errorMessage: 'Required field',
+        },
+        {
+          type: 'minLength',
+          value: 8,
+          errorMessage: 'This password too short',
+        },
+        {
+          type: 'maxLength',
+          value: 20,
+          errorMessage: 'This password too long',
+        },
+      ],
+    },
+    {
+      type: 'number',
+      name: 'age',
+      label: 'Your age',
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FbModule } from 'ngfb';
+import { AngularFormGetformIoModule } from 'angular-form-getform-io';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FbModule],
+  imports: [BrowserModule, AngularFormGetformIoModule],
   providers: [],
   bootstrap: [AppComponent],
 })
